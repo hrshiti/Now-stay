1a9f9ce13d832cc52ef22bf52b3a80da4662cd2a
hrshiti <hritik45raghuwanshi@gmail.com>
2026-02-18 19:24:37 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,9 @@ const io = new Server(server, {
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'https://rukkoo.in',
-      'https://www.rukkoo.in',
-      'https://rukkoo-project.vercel.app'
+      'https://nowstay.in',
+      'https://www.nowstay.in',
+      'nowstay.in'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
