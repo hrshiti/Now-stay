98f93ffd99abaac96dfed8cf07473983b15e2e64
hrshiti <hritik45raghuwanshi@gmail.com>
2026-02-18 19:39:11 +0530

    cors problem

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,9 @@ app.use(cors({
       'http://127.0.0.1:5173',
       'https://rukkoo.in',
       'https://www.rukkoo.in',
-      'https://rukkoo-project.vercel.app'
+      'https://rukkoo-project.vercel.app',
+      'https://nowstay.in',
+      'https://www.nowstay.in'
     ];
     // Add 172.16-31 range (often used by hotspots) and 10.x
     const isLocalNetwork =
